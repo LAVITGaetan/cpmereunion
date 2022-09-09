30b568148b9a92449c754a4402b45c76fd19e443
LAVITGaetan <g.lavit@outlook.fr>
2022-09-09 20:44:26 +0400

    ajout d axios, ejs et de la premiere vue index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fetch = import('node-fetch');
+const axios = require('axios');
 const app = express();
 require('dotenv').config();
 const adherentRoute = require('./routes/adherents');
@@ -8,22 +10,47 @@ const PORT = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-//routes
+// Set view engine
+app.set('view engine', 'ejs')
+
+// Static Files
+app.use(express.static('public'));
+
+// API ROUTES
 app.use('/api/adherents', adherentRoute);
+
+
+// ROUTES
+app.use('/accueil', async (req, res) => {
+    let api_uri = 'http://cpmereunion.herokuapp.com/api/adherents';
+    let adherents = [];
+
+    try {
+        let response = await axios.get(api_uri)
+        console.log(response.data);
+        response.data.forEach(adherent => {
+            adherents.push(adherent)
+            console.log(adherent);
+        });
+        res.render('pages/accueil', { title: 'Hello world', adherents: adherents })
+    } catch (error) {
+        console.error(error)
+    }
+})
 app.use('/', (req, res) => {
-    res.send({message : 'hello'})
+    res.send({ message: 'hello' })
 })
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI,
-{useNewUrlParser:true})
-.then(() => {
-    console.log('connected to database');
-}).catch(error => {
-    console.log(`an error happened : ${error}`);
-})
+    { useNewUrlParser: true })
+    .then(() => {
+        console.log('connected to database');
+    }).catch(error => {
+        console.log(`an error happened : ${error}`);
+    })
 
 // start server
 app.listen(PORT, () => {
