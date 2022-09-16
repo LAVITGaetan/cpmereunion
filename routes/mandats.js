a0b320c73e26f0d3dac4526de572d39bf110eff4
Gaetan LAVIT <g.lavit@outlook.fr>
2022-09-16 11:41:40 +0400

    correction bug

--- routes/mandats.js
+++ routes/mandats.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const Mandat = require('../models/mandat.model');
+const Mandat = require('../models/mandats');
 
 // Retrieve all mandats
 router.get('/', async (req, res) => {
